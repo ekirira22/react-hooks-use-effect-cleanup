3ac3cf1bb8b6cfdb0dcfd7997df8d5e4ee37bf4c
Eric Maranga <ekirira22@gmail.com>
2024-02-07 07:26:26 +0300

    completed

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,15 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+      //Assign the process a const
+    const timerID = setInterval(() => {
       setTime(new Date());
-    }, 1000);
+    }, 1000)
+
+      //return clean-up function
+    return function cleanup(){
+      clearInterval(timerID)
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
